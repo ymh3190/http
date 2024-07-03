1dee75cfe388368f4c06e0ba4383650424dc026e
ymh3190 <ymh3190@gmail.com>
2024-07-03 23:33:59 +0900

    DDoS

--- backend/src/controller.js
+++ backend/src/controller.js
@@ -14,6 +14,7 @@ import {
 import * as CustomError from "./error";
 import util from "./util";
 
+// @ts-check
 class AuthController {
   async signup(req, res) {
     const { username, password } = req.body;
